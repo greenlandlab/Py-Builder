7b5152cf2c287f6ec6b1e6ee5c94a73694ce9f6c
Green Github Dev <112767901+greenlandlab@users.noreply.github.com>
2024-11-30 23:35:41 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,3 @@
 mv py-builder /opt
 mv pybuilder.service /etc/systemd/system
-chcon -t systemd_unit_file_t /etc/systemd/system/pybuilder.service
-systemctl daemon-reload
-systemctl verify pybuilder.service
-systemctl enable pybuilder.service
-echo "Finished now please reboot your system incase"
+echo "returned <0> installed sucessfuly"
