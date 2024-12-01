3ded0b84e0562a3bd069ffbad64f2d6ba057ea4c
Green Github Dev <112767901+greenlandlab@users.noreply.github.com>
2024-12-01 14:43:37 +0600

    Update cmd-adjust-pybuilder.sh

--- py-builder/cmd-adjust-pybuilder.sh
+++ py-builder/cmd-adjust-pybuilder.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 alias py-builder-install='python3 /etc/py-builder/Install_image.py'
-alias pt-builder='python3 /etc/py-builder/Make_Env.py'
+alias py-builder='python3 /etc/py-builder/Make_Env.py'
 tail -f /dev/null
